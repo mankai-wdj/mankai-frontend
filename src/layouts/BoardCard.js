b4ae20f6d436b3b40f6f3557cf1b7f2dc1c079fd
tjdrb63 <tjdrb63@gmail.com>
2022-04-17 02:03:56 +0900

    useEffect 조건추가

--- src/layouts/BoardCard.js
+++ src/layouts/BoardCard.js
@@ -42,10 +42,7 @@ function BoardCard(props){
     const [sampleComment, setSampleComment] = useState([])
     const [titlefieldvalue,setTitleFieldValue] = useState("");
     const [commentLength, setCommentLength] = useState("")
-    const option = ["번역하기","클립보드로 이동",
-    (user.id===Number(props.board.user_id)) ? "수정하기" : null,
-    (user.id===Number(props.board.user_id)) ? "삭제하기" : null
-    ]
+    const option = ["번역하기","클립보드로 이동"];
     const [translated,setTranslated] = useState("");
     const [profile,setProfile] = useState("");
     const [editModalOpen,setEditModalOpen] = useState(false)
@@ -317,9 +314,15 @@ function BoardCard(props){
                                 >
                                     {option.map((option)=>{
                                         return(
-                                            <MenuItem onClick={handleClose}>{option}</MenuItem>
+                                            <MenuItem onClick={handleClose}>{option}</MenuItem> 
                                         )
                                     })}
+                                    {user &&
+                                        user.id===Number(props.board.user_id) && <MenuItem onClick={handleClose}>{option}</MenuItem> 
+                                    }
+                                    {user &&
+                                        user.id===Number(props.board.user_id) && <MenuItem onClick={handleClose}>{option}</MenuItem> 
+                                    }
                                     
                                 </Menu>
                             </div>
