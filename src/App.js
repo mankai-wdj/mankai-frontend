e52b720ff61582b5a7797adf9490be90285f1d39
woduf9173@naver.com <woduf9173@naver.com>
2022-04-18 16:19:00 +0900

    .

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ import YouPage from './components/MyPage/YouPage'
 import GroupDetail from './components/GroupDetail'
 import firebase from 'firebase/app';
 import "firebase/messaging"
-axios.defaults.baseURL = 'http://mankai.shop:8000/'
+axios.defaults.baseURL = 'http://api.mankai.shop:8080/'
 axios.defaults.headers.post['Content-Type'] = 'application/json'
 axios.defaults.headers.post['Accept'] = 'application/json'
 axios.defaults.withCredentials = true
