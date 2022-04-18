ae0e7bbb84a8b09459c4ebaf4157a89bd9bf7ced
chli-jiseong <chlwltjd98@naver.com>
2022-04-18 10:25:01 +0900

    commit

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -127,6 +127,9 @@ const initialState = {
   followerFollower: null,
   followings: null,
   followerFollowing: null,
+  room_files : [],
+  room_images : [],
+  room_memos : [],
 }
 
 export default handleActions(
